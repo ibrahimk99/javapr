75ec6a275f239701893fe3281ea864f5ef1ee108
ibrahimk99 <ibrahimkhanbrt@gmail.com>
2026-04-29 09:28:49 +0500

    starting Binary Search Tree

--- BinaryTrees3.java
+++ BinaryTrees3.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 import org.w3c.dom.Node;
 
 public class BinaryTrees3 {
@@ -28,14 +30,147 @@ public class BinaryTrees3 {
     }
   }
 
-  public static void kthLevel(Node root) {
+  public static void kthLevel(Node root, int level, int k) {
+    if (root == null) {
+      return;
+    }
+    if (level == k) {
+      System.out.print(root.data + " ");
+    }
+    kthLevel(root.left, level + 1, k);
+    kthLevel(root.right, level + 1, k);
+  }
 
+  public static boolean getPath(Node root, int n, ArrayList<Node> path) {
+    if (root == null) {
+      return false;
+    }
+    path.add(root);
+    if (root.data == n) {
+      return true;
+    }
+    boolean foundLeft = getPath(root.left, n, path);
+    boolean foundRight = getPath(root.right, n, path);
+    if (foundLeft || foundRight) {
+      return true;
+    }
+    path.remove(path.size() - 1);
+    return false;
+  }
+
+  public static Node lca(Node root, int n1, int n2) {
+    ArrayList<Node> path1 = new ArrayList<>();
+    ArrayList<Node> path2 = new ArrayList<>();
+    getPath(root, n1, path1);
+    getPath(root, n2, path2);
+    // last common ancestor
+    int i = 0;
+    for (; i < path1.size() && i < path2.size(); i++) {
+      if (path1.get(i) != path2.get(i)) {
+        break;
+      }
+    }
+    // last equal node ->i-1th index
+    Node lca = path1.get(i - 1);
+    return lca;
+  }
+
+  public static Node lca2(Node root, int n1, int n2) {
+    if (root == null || root.data == n1 || root.data == n2) {
+      return root;
+    }
+    Node leftLca = lca2(root.left, n1, n2);
+    Node rightLca = lca2(root.right, n1, n2);
+    // leftLca=val; rightlca=null;
+    if (rightLca == null) {
+      return leftLca;
+    }
+    // leftLca=null; rightlca=val ;
+    if (leftLca == null) {
+      return rightLca;
+    }
+    return root;
+  }
+
+  public static int lcaDist(Node root, int n) {
+    if (root == null) {
+      return -1;
+    }
+    if (root.data == n) {
+      return 0;
+    }
+    int leftDist = lcaDist(root.left, n);
+    int rightDist = lcaDist(root.right, n);
+    if (leftDist == -1 && rightDist == -1) {
+      return -1;
+    } else if (leftDist == -1) {
+      return rightDist + 1;
+    } else {
+      return leftDist + 1;
+    }
+  }
+
+  public static int minDistance(Node root, int n1, int n2) {
+    Node lca = lca2(root, n1, n2);
+    int dist1 = lcaDist(lca, n1);
+    int dist2 = lcaDist(lca, n2);
+    return dist1 + dist2;
+
+  }
+
+  public static int KthAncestor(Node root, int n, int k) {
+    if (root == null) {
+      return -1;
+    }
+    if (root.data == n) {
+      return 0;
+    }
+    int leftDist = KthAncestor(root.left, n, k);
+    int rightDist = KthAncestor(root.right, n, k);
+    if (leftDist == -1 && rightDist == -1) {
+      return -1;
+    }
+    int max = Math.max(leftDist, rightDist);
+    if (max + 1 == k) {
+      System.out.println(root.data);
+    }
+    return max + 1;
+  }
+
+  public static int transform(Node root) {
+    if (root == null) {
+      return 0;
+    }
+    int leftChild = transform(root.left);
+    int rightChild = transform(root.right);
+
+    int data = root.data;
+    int newLeft = root.left == null ? 0 : root.left.data;
+    int newRight = root.right == null ? 0 : root.right.data;
+    root.data = newLeft + leftChild + newRight + rightChild;
+    return data;
+  }
+
+  public static void preOrder(Node root) {
+    if (root == null) {
+      return;
+    }
+    System.out.println(root.data);
+    preOrder(root.left);
+    preOrder(root.right);
   }
 
   public static void main(String[] args) {
     int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, 6, -1, -1, 7, -1, -1 };
     BinaryTree tree = new BinaryTree();
     Node root = tree.buildTree(nodes);
-    System.out.println(root.data);
+    // kthLevel(root, 1, 3);
+    int n1 = 4, n2 = 7;
+    // System.out.println(lca(root, n1, n2).data);
+    // System.out.println(lca2(root, n1, n2).data);
+    // System.out.println(minDistance(root, n1, n2));
+    // System.out.println(KthAncestor(root, 4, 1));
+    transform(root);
+    preOrder(root);
   }
 }
